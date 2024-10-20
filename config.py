48699fd20fe99ced018a10649dc38f26f0e0ad6c
Animepahe498 <148706595+Animepahe498@users.noreply.github.com>
2024-10-20 14:19:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class Config(object):
 
     # wes response configuration
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-    PORT = int(os.environ.get("PORT", "8010"))
+    PORT = int(os.environ.get("PORT", "8080"))
 
 
 class TxT(object):
